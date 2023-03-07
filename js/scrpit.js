5c2747ef79cfaae946c4d4d5a17b73a046311592
tlucass <rm97913@fiap.com.br>
2023-03-07 11:49:01 -0300

    script.js adicionado ao html.

---
+++ js/scrpit.js
@@ -0,0 +1,15 @@
+//este é um comentário de linha
+
+/**
+ * 
+ * é
+ * um comentário
+ * de bloco
+ */
+
+
+//Exemplo das funções do console.
+console.log("Olá Mundo!")
+console.error("Bom dia!")
+console.warn("Será?")
+console.info("Vai ser...")
